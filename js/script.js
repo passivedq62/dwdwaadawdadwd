2652fcfea5f5cb0f15150892403f37f0764d383a
passivedq62 <sketchsint@mailfence.com>
2024-10-04 07:06:33 +0845

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function startTyping() {
 
      
       setTimeout(() => {
-        window.location.href = '/homepage.html';  
+        window.location.href = 'homepage.html';  
       }, 1000); 
     }
   }, 100); 
